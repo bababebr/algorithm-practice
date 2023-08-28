bec0821284e9d1a1291805c88be8f0e880625532
bababebr <unior1234@yandex.ru>
2023-08-28 09:47:45 +0600

    Initial commit.

---
+++ Easy/src/main/java/org/example/algorithm/Zip.java
@@ -0,0 +1,60 @@
+package org.example.algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Zip {
+
+    private static List<Integer> zip(List<Integer> a, List<Integer> b) {
+        int iter = a.size() + b.size() - 1;
+        Integer[] array = new Integer[iter + 1];
+        for (int i = 0, k = 0, j = 0; j <= iter; j++) {
+            if (j == 0 || j % 2 == 0) {
+                array[j] = a.get(i);
+                i++;
+            } else {
+                array[j] = b.get(k);
+                k++;
+            }
+        }
+        return List.of(array);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int n = Integer.parseInt(reader.readLine());
+            List<Integer> a = readList(reader);
+            List<Integer> b = readList(reader);
+            printList(zip(a, b), writer);
+        }
+    }
+
+    private static List<Integer> readList(BufferedReader reader) throws IOException {
+        return Arrays.asList(reader.readLine().split(" "))
+                .stream()
+                .map(elem -> Integer.parseInt(elem))
+                .collect(Collectors.toList());
+    }
+
+    private static <T> void printList(List<T> list, Writer writer) {
+        list.forEach(elem -> {
+                    try {
+                        writer.write(String.valueOf(elem));
+                        writer.write(" ");
+                    } catch (IOException e) {
+
+                    }
+                }
+        );
+    }
+}
