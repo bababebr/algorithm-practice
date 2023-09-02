e3113359d208ec8049a906efd8968d584e152078
bababebr <unior1234@yandex.ru>
2023-09-02 09:42:36 +0600

    feat: ReverseWords

--- Easy/src/main/java/org/example/leet75/arraysString/MergeWords.java
+++ Easy/src/main/java/org/example/leet75/arraysString/MergeWords.java
@@ -5,11 +5,11 @@ public class MergeWords {
     public static String mergeAlternately(String word1, String word2) {
         StringBuilder sb = new StringBuilder();
         int totalLength = word1.length() + word2.length();
-        for(int i = 0; i < totalLength; i++) {
-            if(i < word1.length()){
+        for (int i = 0; i < totalLength; i++) {
+            if (i < word1.length()) {
                 sb.append(word1.charAt(i));
             }
-            if(i < word2.length()) {
+            if (i < word2.length()) {
                 sb.append(word2.charAt(i));
             }
         }
---
+++ Easy/src/main/java/org/example/leet75/arraysString/ReverseWordsInString.java
@@ -0,0 +1,10 @@
+package org.example.leet75.arraysString;
+
+public class ReverseWordsInString {
+
+
+    public static void main(String[] args) {
+        String s = "s     s";
+        s.repl
+    }
+}
