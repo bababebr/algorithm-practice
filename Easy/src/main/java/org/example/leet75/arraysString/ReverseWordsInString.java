5b28c5efd18363df6aec1eff7ff0b08eead1dcb6
bababebr <unior1234@yandex.ru>
2023-09-03 21:40:41 +0600

    feat: ReverseWords

--- Easy/src/main/java/org/example/leet75/arraysString/ReverseWordsInString.java
+++ Easy/src/main/java/org/example/leet75/arraysString/ReverseWordsInString.java
@@ -2,10 +2,13 @@ package org.example.leet75.arraysString;
 
 public class ReverseWordsInString {
 
-
-    public static void main(String[] args) {
-        String s = "s     s    q";
-        s = s.replaceAll("\\s+", " ");
-        System.out.println(s);
+    public String reverseWords(String s) {
+        String[] splitString = s.trim().replaceAll("\\s+", " ").split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = splitString.length - 1; i >= 0 ; i--) {
+            sb.append(splitString[i]);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
     }
 }
