b94121767f34440e1a01f3012aa1dc1de267003a
bababebr <unior1234@yandex.ru>
2023-09-05 21:53:47 +0600

    feat: ReverseWords

--- Easy/src/main/java/org/example/leet75/arraysString/TripletArray.java
+++ Easy/src/main/java/org/example/leet75/arraysString/TripletArray.java
@@ -4,14 +4,10 @@ import java.util.TreeMap;
 
 public class TripletArray {
 
+/*
     public static boolean increasingTriplet(int[] nums) {
 
     }
+*/
 
-
-
-    public static void main(String[] args) {
-        int[] array = new int[]{2, 1, 5, 0, 4, 6};
-        System.out.println(increasingTriplet(array));
-    }
 }
