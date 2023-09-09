8643f66aac91405bed0bfd86af7efae50438a06a
bababebr <unior1234@yandex.ru>
2023-09-09 09:31:20 +0600

    feat: isSubsequence

---
+++ Easy/src/main/java/org/example/leet75/twoPointers/IsSubsequence.java
@@ -0,0 +1,9 @@
+package org.example.leet75.twoPointers;
+
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        for(char c : s.charAt()) {
+
+        }
+    }
+}
