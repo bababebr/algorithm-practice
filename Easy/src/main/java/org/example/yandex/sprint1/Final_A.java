55702c263e4387fe5abc1f5371b06ce9038e788f
bababebr <unior1234@yandex.ru>
2023-09-17 16:55:09 +0600

    feat: Final A slow

--- Easy/src/main/java/org/example/yandex/sprint1/Final_A.java
+++ Easy/src/main/java/org/example/yandex/sprint1/Final_A.java
@@ -7,25 +7,28 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Final_A {
-
+    /**
+     * Slow - refactoring required.
+     * @param arr
+     * @return
+     */
     public static String zero(List<Integer> arr) {
         TreeMap<Integer, Integer> zerosIdx = new TreeMap<>();
         StringBuilder distances = new StringBuilder();
-
         for (int i = 0; i < arr.size(); i++) {
             if (arr.get(i) == 0) {
                 zerosIdx.put(i, 0);
             }
         }
-
         int minDist;
-
+        Integer lower;
+        Integer higher;
         for (int i = 0; i < arr.size(); i++) {
             if (zerosIdx.containsKey(i)) {
                 distances.append(0).append(" ");
             } else {
-                Integer lower = zerosIdx.lowerKey(i);
-                Integer higher = zerosIdx.higherKey(i);
+                lower = zerosIdx.lowerKey(i);
+                higher = zerosIdx.higherKey(i);
                 if (higher == null) {
                     minDist = i - lower;
                 } else if (lower == null) {
