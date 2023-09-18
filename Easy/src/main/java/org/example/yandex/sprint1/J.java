7aa21d338f1eca01750bcec222dcc89e083582cb
bababebr <unior1234@yandex.ru>
2023-09-18 20:00:38 +0600

    feat: Final B

--- Easy/src/main/java/org/example/yandex/sprint1/J.java
+++ Easy/src/main/java/org/example/yandex/sprint1/J.java
@@ -5,13 +5,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class J {
 
     private static List<Integer> factorize(int n) {
+        System.out.println(isPrime(4));
         return null;
     }
+
+    private static boolean isPrime(int n) {
+        for (int i = 2; i <= n; i++) {
+            if ((n % i) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
