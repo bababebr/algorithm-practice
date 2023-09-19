60c6482413a7164ecc6518f4d59556e5ee65e792
bababebr <unior1234@yandex.ru>
2023-09-19 15:20:17 +0600

    feat: Sprint 1 Done

--- Easy/src/main/java/org/example/yandex/sprint1/Final_B.java
+++ Easy/src/main/java/org/example/yandex/sprint1/Final_B.java
@@ -10,24 +10,24 @@ public class Final_B {
     public static int maxScore(int k, ArrayList<String> field) {
         int score = 0;
         int[] counter = new int[10];
-        for(int i = 0; i < 4; i++) {
-            for(char c : field.get(i).toCharArray()) {
-                if(c == '.'){
+        for (int i = 0; i < 4; i++) {
+            for (char c : field.get(i).toCharArray()) {
+                if (c == '.') {
                     continue;
                 }
-                int num = Integer.parseInt(String.valueOf(c));
-                counter[num] = ++counter[num];
+                int num = Character.digit(c, 10);
+                ++counter[num];
             }
         }
 
-        for(int i = 0; i < 10; i++) {
-            if(counter[i] <= k*2 && counter[i] > 0) score++;
+        for (int i = 0; i < 10; i++) {
+            if (counter[i] <= k * 2 && counter[i] > 0) score++;
         }
         return score;
     }
 
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             int k = readInt(reader);
             ArrayList<String> field = readArray(reader);
             System.out.println(maxScore(k, field));
@@ -38,7 +38,7 @@ public class Final_B {
 
     private static ArrayList<String> readArray(BufferedReader reader) throws IOException {
         ArrayList<String> field = new ArrayList<>();
-        for(int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) {
             field.add(reader.readLine());
         }
         return field;
