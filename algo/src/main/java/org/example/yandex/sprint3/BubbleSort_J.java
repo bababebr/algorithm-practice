72e099ee0728eb9f63333b83a5c4cfafa8be10a9
bababebr <unior1234@yandex.ru>
2023-10-14 21:50:13 +0600

    feat: Spring 3 BubbleSort_J

---
+++ algo/src/main/java/org/example/yandex/sprint3/BubbleSort_J.java
@@ -0,0 +1,97 @@
+package org.example.yandex.sprint3;
+
+import org.example.yandex.sprint1.B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSort_J {
+
+/*    public static Integer[] bubbleSort_opt2(Integer[] array) {
+        StringBuilder sb = new StringBuilder();
+        int max = array.length - 1;
+        boolean check;
+        for (int i = 0; i < array.length; i++) {
+            check = false;
+            for (int j = 0; j < max; j++) {
+                if (array[j] > array[j + 1]) {
+                    check = true;
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+            if (!check) return array;
+            sb.append(toString(array));
+            System.out.println(sb);
+            sb = new StringBuilder();
+            max--;
+        }
+        return array;
+    }*/
+
+    public static Integer[] bubbleSort_opt(Integer[] array) {
+        StringBuilder sb = new StringBuilder();
+        int max = array.length - 1;
+        boolean check;
+        for (int i = 0; i < array.length; i++) {
+            check = false;
+            for (int j = 0; j < max; j++) {
+                if (array[j] > array[j + 1]) {
+                    check = true;
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+            sb.append(toString(array));
+            System.out.println(sb);
+            if (!check) {
+                return array;
+            }
+            sb = new StringBuilder();
+            max--;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = readInt(reader);
+            bubbleSort_opt(readArray(reader));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public static int readInt(BufferedReader reader) throws IOException {
+        return Integer.parseInt(reader.readLine());
+    }
+
+    public static Integer[] readArray(BufferedReader reader) throws IOException {
+        return Arrays.stream(reader.readLine().split(" ")).map(e -> Integer.parseInt(e)).toArray(Integer[]::new);
+    }
+
+
+    public static int[] generateArray(int n) {
+        Random rnd = new Random();
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = rnd.nextInt(n);
+        }
+        return result;
+    }
+
+    public static String toString(Integer[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int i : array) {
+            sb.append(i).append(" ");
+        }
+        return sb.toString();
+    }
+}
