4467af0b69bdd1459a76f7a00e06d5a7067d6744
bababebr <unior1234@yandex.ru>
2023-10-14 09:12:41 +0600

    feat: Spring 3 Subsequence_C

--- algo/src/main/java/org/example/yandex/sprint3/Subsequence_C.java
+++ algo/src/main/java/org/example/yandex/sprint3/Subsequence_C.java
@@ -1,7 +1,34 @@
 package org.example.yandex.sprint3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Subsequence_C {
 
+    public static boolean isSub(String s, String t) {
+        if (s.length() > t.length()) return false;
+        int j = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(j) == t.charAt(i)) {
+                j++;
+            }
+            if (j >= s.length()) return true;
+        }
+        return false;
+    }
 
-
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String s = reader.readLine();
+            String t = reader.readLine();
+            if (isSub(s, t)) {
+                System.out.println("True");
+            } else {
+                System.out.println("False");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
