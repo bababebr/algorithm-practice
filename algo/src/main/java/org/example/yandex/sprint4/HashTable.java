424a5439f5322c23e55d94cf1387580024a68e81
bababebr <solovevworkmail@gmail.com>
2023-11-07 14:24:01 +0300

    feat: sprint 4 - Final B - Final

--- algo/src/main/java/org/example/yandex/sprint4/HashTable.java
+++ algo/src/main/java/org/example/yandex/sprint4/HashTable.java
@@ -6,10 +6,41 @@ import java.io.InputStreamReader;
 import java.util.Objects;
 
 public class HashTable {
+    /*
+    -- ПРИНЦИП РАБОТЫ --
+    Посылка - 96236429
+
+    Класс хэш-мапы - Map состоит из константы размера корзин BUCKET_SIZE, массива Нод "table".
+    Класс Ноды - Node<V,K> содержит V - value, K - key, next - след. нода.
+
+    При инициализании Хэш-таблицы создается массив table и запоняется null-ми.
+    Когда мы добавляем эл-т, внутри мапы вызывается метод bucket(hash(key)), где hash() -> метод получениях хэша.
+
+    При возникновении коллизий они устраняются методом цепочек.
+
+    Размер мапы выбран в размере 133337 т.к. по условию задачи в мапе не может быть более 10^5 эл-тов. Ближайшее простое
+    к 10^5 - это 100003, делим его на коэффициент масштабирования 0.75 и получаем 133337.
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+
+    Алгоритм хэширования является детерминированным т.к. состоит только и суммирования и побитовых операторов.
+    Это гарантирует, что для одного и того же ключа будет выдаваться одинаковый хэш.
+
+    Метод цепочек позволяет избежать коллизий т.к. эл-ты с одинаковым хэшем будут храниться в виде связанного списка.
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+
+    Считать команды -> O(n)
+    Создать мапу фиксированного размера - O(1)
+    Положить и достать эл-т в лучшем и среднем случаее O(1), худший случай возможен при неравномерном распределении хеша,
+    в таком случае все эл-ты будут попадать в одну корзину. Тогда необходимо будет постоянно проходиться по списку, и
+    удаление, добавление и поиск будет занимать O(n) времени.
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+        Размер мапы выбран 1.25 * m, где m - максммальное кол-во эл-тов в мапе. Отсюда сложность в лучшем случаее O(m)
+     */
 
     public static void main(String[] args) {
-        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))
-        //BufferedReader reader = new BufferedReader(new FileReader(new File("C:\\Users\\unior1234\\IdeaProjects\\algorithm-practice\\algo\\src\\main\\resources\\input_18.txt")))
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             Map map = new Map();
             int n = readInt(reader);
@@ -17,13 +48,10 @@ public class HashTable {
             for (int i = 0; i < n; i++) {
                 String[] command = reader.readLine().split(" ");
                 long result = 0;
-                //System.out.println(command[0] + " " + command[1]);
                 switch (command[0]) {
                     case "put":
                         map.put(Integer.parseInt(command[1]), Long.parseLong(command[2]));
-                       /* map.printBucket(Integer.valueOf(command[1]));
-                        System.out.println("---------------");
-                       */ break;
+                        break;
                     case "delete":
                         result = map.delete(Integer.parseInt(command[1]));
                         if (result != -1) {
@@ -53,9 +81,9 @@ public class HashTable {
     }
 
     static class Map {
-        private final static int BUCKET_SIZE = 61633337;
+        private final static int BUCKET_SIZE = 133337;
         private final Node<Integer, Long>[] table = new Node[BUCKET_SIZE];
-        public long size = 0;
+
 
         public void put(Integer key, Long value) {
             Node<Integer, Long> newNode = new Node<>(key, value);
@@ -75,31 +103,12 @@ public class HashTable {
                 }
                 if (Objects.equals(node.key, key)) {
                     node.value = value;
-                    return;
                 } else {
                     node.next = newNode;
-                    return;
                 }
-/*                while (node.hasNext()) {
-                    if (Objects.equals(node.key, key)) {
-                        node.value = value;
-                        break;
-                    }
-                    node = node.next;
-                }
-
-                node.next = newNode;*/
+                return;
             }
-            size++;
             table[bucket] = newNode;
-        }
-
-        public void printBucket(Integer key) {
-            Node<Integer, Long> node = table[bucket(key)];
-            while (node != null) {
-                System.out.println(node.key + " " + node.value);
-                node = node.next;
-            }
         }
 
         public long get(int key) {
@@ -120,37 +129,38 @@ public class HashTable {
             Node<Integer, Long> node = table[bucket];
             Node<Integer, Long> prev = null;
             if (node == null) return -1;
+
             if (Objects.equals(node.key, key)) {
-                size--;
                 table[bucket] = node.next;
                 return node.value;
             }
+
             while (node != null && !Objects.equals(node.key, key)) {
                 prev = node;
                 node = node.next;
             }
+
             if (node == null)
                 return -1;
 
             prev.next = node.next;
-            size--;
             return node.value;
         }
 
+        //Получение номера корзины из хэша с помощью получения остатка от деления на размер мапы.
         private int bucket(Integer key) {
             return (int) (hash(key) % BUCKET_SIZE);
-            //return 1;
         }
 
+        //Алгоритм получения хэша по ключу, взят из офф документации по Java 7
         private long hash(Integer key) {
-            key = key + 1_000_000_000;
+            key = key + 1_000_000_000; // чтобы исключить отрицательные индексы.
             key ^= (key >>> 20) ^ (key >>> 12);
             return key ^ (key >>> 7) ^ (key >>> 4);
         }
     }
 
     static class Node<K, V> {
-        int hash;
         K key;
         V value;
         Node<K, V> next;
@@ -163,7 +173,5 @@ public class HashTable {
             this.key = key;
             this.value = value;
         }
-
     }
-
 }
